81c8ff32f84af8551cfcf850fdd0c639a88db0ac
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-19 10:31:20 +0100

    added app starter code for routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -23,3 +23,13 @@ function App() {
 }
 
 export default App;
+
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/events" element={<Events />} />
+    <Route path="/artists" element={<Artists />} />
+  </Routes>
+</BrowserRouter>;
