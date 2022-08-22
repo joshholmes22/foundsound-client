826ad5642dcf1388a2decf55bc50fa36561f6fb7
Josh Holmes <josh@glowingmanagement.com>
2022-08-22 06:40:53 +0100

    signup buttons template

---
+++ src/components/AccountTypeCard.js
@@ -0,0 +1,44 @@
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+const AccountTypeCard = ({ imageUrl, altText, overlayText }) => {
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        margin: "10px",
+        backgroundColor: "transparent",
+        width: "300px",
+      }}
+    >
+      <Box sx={{ position: "relative" }}>
+        <CardMedia
+          component="img"
+          height="200"
+          image={imageUrl}
+          alt={altText}
+          sx={{ borderRadius: "15px" }}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            bgcolor: "rgba(0, 0, 0, 0.7)",
+            color: "white",
+            padding: "10px",
+          }}
+        >
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            {overlayText}
+          </Typography>
+        </Box>
+      </Box>
+    </Card>
+  );
+};
+
+export default AccountTypeCard;
