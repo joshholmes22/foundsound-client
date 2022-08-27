b9340cd10b6b83a387148730fc9b6841d4f3e231
Josh Holmes <josh@glowingmanagement.com>
2022-08-27 13:45:20 +0100

    link apollo client

--- src/App.js
+++ src/App.js
@@ -5,25 +5,48 @@ import {
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Events from "./pages/Events";
 import Artists from "./pages/Artists";
-
 import "./App.css";
+
+const link = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL || "http://localhost:4000",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/artists" element={<Artists />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/artists" element={<Artists />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
