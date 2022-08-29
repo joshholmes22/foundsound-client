52dad117d4a7373ba9e4a7048a88760c3ccf4dad
Josh Holmes <josh@glowingmanagement.com>
2022-08-29 13:01:26 +0100

    Dashboard containers

---
+++ src/containers/ArtistDashboard/ArtistDashboard.js
@@ -0,0 +1,11 @@
+import "./ArtistDashboard.css";
+
+const ArtistDashboard = () => {
+  return (
+    <div>
+      <h1>Artist Dashboard</h1>
+    </div>
+  );
+};
+
+export default ArtistDashboard;
---
+++ src/containers/EventOrganiserDashboard/EventOrganiserDashboard.js
@@ -0,0 +1,11 @@
+import "./EventOrganiserDashboard.css";
+
+const EventOrganiserDashboard = () => {
+  return (
+    <div>
+      <h1>Event Organiser Dashboard</h1>
+    </div>
+  );
+};
+
+export default EventOrganiserDashboard;
