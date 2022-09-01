54ce40e83dfd3870a34dd1e112bd3fb295583c05
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-01 20:44:23 +0100

    created theme

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   typography: { fontFamily: "Montserrat" },
   palette: {
-    primary: { main: "#0288d1" },
+    primary: { main: "#4A69FF" },
   },
 });
 
