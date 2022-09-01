d373a742c6ec12b7db176e22bf94272cb0d18cd6
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-01 20:34:11 +0100

    changed button colour

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -2,6 +2,9 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: { fontFamily: "Montserrat" },
+  palette: {
+    primary: { main: "#0288d1" },
+  },
 });
 
 export default theme;
