b1b01f0826d8d20d91704dda6785e1b202fd94bf
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-09-01 21:27:42 +0100

    nav bar component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import { Typography } from "@mui/material";
 import Link from "@mui/material/Link";
+import HomeNavBar from "../../components/HomeNavBar";
 
 import "./Home.css";
 
 const Home = () => {
   return (
     <div>
+      <HomeNavBar />
       <Typography>HOME</Typography>
       <Link href="/signup">Click here to sign up</Link>
     </div>
