40f832ae04015c26ed7e728d2ee6654fc8f36582
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-03 11:02:03 +0100

    inital commit - designing create event form

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,7 +1,23 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
 import "./Events.css";
 
 const Events = () => {
-  return <h1>Events</h1>;
+  return (
+    <div>
+      <h1 className="page-title">Create an Event</h1>
+      <React.Fragment>
+        <CssBaseline />
+        <Container className="event-container" maxWidth="sm">
+          <Box sx={{ bgcolor: "#f8f9fa", height: "100vh" }} />
+          <div></div>
+        </Container>
+      </React.Fragment>
+    </div>
+  );
 };
 
 export default Events;
