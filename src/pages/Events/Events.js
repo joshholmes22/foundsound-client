39f1ef63658875763478700ea99deb08dc0f3f48
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-03 12:21:27 +0100

    removed auther css using mui

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,22 +1,16 @@
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import { ThemeProvider } from "@mui/material/styles";
+
+import theme from "../../utils/themes";
 
 import "./Events.css";
 
 const Events = () => {
   return (
-    <div>
-      <h1 className="page-title">Create an Event</h1>
-      <React.Fragment>
-        <CssBaseline />
-        <Container className="event-container" maxWidth="sm">
-          <Box sx={{ bgcolor: "#f8f9fa", height: "100vh" }} />
-          <div></div>
-        </Container>
-      </React.Fragment>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs"></Container>
+    </ThemeProvider>
   );
 };
 
