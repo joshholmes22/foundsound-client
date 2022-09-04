06748d9d020b98bd85103b1204c900ff28d616e7
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-04 13:51:22 +0100

    create button added and styled

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
@@ -35,55 +37,72 @@ const Events = () => {
           Add Event Information
         </Typography>
 
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              required
-              id="EventName"
-              name="EventName"
-              label="Event Name"
-              fullWidth
-              autoComplete="given-name"
-              variant="standard"
-            />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="EventName"
+                name="EventName"
+                label="Event Name"
+                fullWidth
+                autoComplete="given-name"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="EventDescription"
+                label="Event Description"
+                fullWidth
+                placeholder="Enter Event Description"
+                multiline
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Stack spacing={3}>
+                  <Grid>
+                    <DesktopDatePicker
+                      xs={{ m: "20px" }}
+                      required
+                      label="Date of Event*"
+                      inputFormat="MM/dd/yyyy"
+                      value={value}
+                      onChange={handleChange}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Grid>
+                  <Grid>
+                    <TimePicker
+                      required
+                      label="Time of Event*"
+                      value={value}
+                      onChange={handleChange}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Grid>
+                </Stack>
+              </LocalizationProvider>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Create Event
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              id="EventDescription"
-              label="Event Description"
-              fullWidth
-              placeholder="Enter Event Description"
-              multiline
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <Stack spacing={3}>
-                <Grid>
-                  <DesktopDatePicker
-                    xs={{ m: "20px" }}
-                    required
-                    label="Date of Event*"
-                    inputFormat="MM/dd/yyyy"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </Grid>
-                <Grid>
-                  <TimePicker
-                    required
-                    label="Time of Event*"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </Grid>
-              </Stack>
-            </LocalizationProvider>
-          </Grid>
-        </Grid>
+        </Box>
       </Container>
     </ThemeProvider>
   );
