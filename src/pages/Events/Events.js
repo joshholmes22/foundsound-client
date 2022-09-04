5c40e3581b493638271411a79d471130d671f47a
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-04 13:33:26 +0100

    date and time pickers addeed

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,17 +1,30 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import Stack from "@mui/material/Stack";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import { ThemeProvider } from "@mui/material/styles";
 
 import theme from "../../utils/themes";
-
 import "./Events.css";
 
 const Events = () => {
+  // const [value, setValue] = React.useState("Controlled");
+  const [value, setValue] = React.useState(new Date("2014-08-18T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Typography variant="h4" gutterBottom align="center" sx={{ m: "20px" }}>
@@ -23,7 +36,7 @@ const Events = () => {
         </Typography>
 
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12}>
             <TextField
               required
               id="EventName"
@@ -34,6 +47,33 @@ const Events = () => {
               variant="standard"
             />
           </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="EventDescription"
+              label="Event Description"
+              fullWidth
+              placeholder="Enter Event Description"
+              multiline
+            />
+          </Grid>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Stack spacing={3}>
+              <DesktopDatePicker
+                label="Date desktop"
+                inputFormat="MM/dd/yyyy"
+                value={value}
+                onChange={handleChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <TimePicker
+                label="Time"
+                value={value}
+                onChange={handleChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </Stack>
+          </LocalizationProvider>
         </Grid>
       </Container>
     </ThemeProvider>
