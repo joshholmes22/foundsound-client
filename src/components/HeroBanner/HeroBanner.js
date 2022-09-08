418d58720d6d4f79fe7c1585a66ea156e7237430
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-09-08 19:29:19 +0100

    made title responsive

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -2,9 +2,6 @@ import "./HeroBanner.css";
 
 const HeroBanner = () => {
   return <h1 className="home-title">foundsound</h1>;
-  {
-    /* <p>Discover underground artists</p>; */
-  }
 };
 
 export default HeroBanner;
