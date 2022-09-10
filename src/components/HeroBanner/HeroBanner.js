8d9a9775fdbeb17d2cd1e517dcff23c1cbb95c19
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-09-10 12:36:42 +0100

    h2

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,7 +1,14 @@
 import "./HeroBanner.css";
 
 const HeroBanner = () => {
-  return <h1 className="home-title">foundsound</h1>;
+  return (
+    <>
+      <h1 className="home-title">foundsound</h1>
+      <h2 className="home-para">
+        Discover underground artists and upcoming events
+      </h2>
+    </>
+  );
 };
 
 export default HeroBanner;
