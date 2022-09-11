a9130a28ca2a11d5e486f95a3ec7bb830e5af86d
Josh Holmes <josh@glowingmanagement.com>
2022-09-11 08:13:36 +0100

    create photo carousel

--- src/components/ArtistBanner/ArtistBanner.js
+++ src/components/ArtistBanner/ArtistBanner.js
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Box from "@mui/material/Box";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import IconButton from "@mui/material/IconButton";
 
-const ArtistBanner = ({ bannerImg }) => {
+import bannerImg from "../../assets/images/profileTemplate/banner-img.jpg";
+import artistImage1 from "../../assets/images/profileTemplate/6J7A5171.jpg";
+import artistImage2 from "../../assets/images/profileTemplate/6J7A5173.jpg";
+
+const ArtistBanner = () => {
+  const artistImages = [bannerImg, artistImage1, artistImage2];
+  const maxSteps = artistImages.length;
+  const [activeStep, setActiveStep] = useState(0);
   const imageUrl =
     "https://images.unsplash.com/photo-1522745287160-f12721561e60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80";
+
+  const handleNext = () => {
+    if (activeStep + 1 !== maxSteps) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    } else {
+      setActiveStep(0);
+    }
+  };
+
+  const handleBack = () => {
+    if (activeStep - 1 !== -1) {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    } else {
+      setActiveStep(maxSteps - 1);
+    }
+  };
   return (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
       <Card>
@@ -23,17 +51,24 @@ const ArtistBanner = ({ bannerImg }) => {
       <Box
         sx={{
           position: "absolute",
+          display: "flex",
         }}
       >
+        <IconButton aria-label="previousImage" onClick={handleBack}>
+          <ArrowCircleLeftIcon fontSize="large" />
+        </IconButton>
         <Card sx={{ maxWidth: "100vw" }}>
           <CardMedia
             component="img"
             height="350vh"
-            image={bannerImg}
+            image={artistImages[activeStep]}
             alt="artist banner"
             sx={{ height: "45vh", width: "auto" }}
           />
         </Card>
+        <IconButton aria-label="nextImage" onClick={handleNext}>
+          <ArrowCircleRightIcon fontSize="large" />
+        </IconButton>
       </Box>
     </Box>
   );
--- src/components/ArtistTracks/ArtistTracks.js
+++ src/components/ArtistTracks/ArtistTracks.js
@@ -2,32 +2,10 @@ import Box from "@mui/material/Box";
 import { useState } from "react";
 import { useTheme } from "@mui/material/styles";
 import MobileStepper from "@mui/material/MobileStepper";
-import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-
-const steps = [
-  {
-    label: "Select campaign settings",
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
-  },
-  {
-    label: "Create an ad group",
-    description:
-      "An ad group contains one or more ads which target a shared set of keywords.",
-  },
-  {
-    label: "Create an ad",
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
-  },
-];
 
 const ArtistTracks = () => {
   const demoSongs = [
