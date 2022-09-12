c7cb02cdfc84824aa0db73b34b6459e43e0bfec0
Josh Holmes <josh@glowingmanagement.com>
2022-09-12 19:30:23 +0100

    add image uploader

--- src/containers/ArtistImageUploader/ArtistImageUploader.js
+++ src/containers/ArtistImageUploader/ArtistImageUploader.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { Box } from "@mui/system";
+import { ImageUploader } from "../../components/ImageUploader";
+import { useAuth } from "../../context/AppProvider";
+
 const ArtistImageUploader = () => {
+  const { user } = useAuth();
+  const [imageUrl, setImageUrl] = useState();
+  const [fileName, setFileName] = useState();
   return (
-    <div>
-      <h1>Image Upload</h1>
-    </div>
+    <Box sx={{ mt: "20%" }}>
+      <ImageUploader
+        imageUrl={imageUrl}
+        setImageUrl={setImageUrl}
+        setFileName={setFileName}
+        dirName={`users/${user.email}/artist`}
+        imageUse="artistImage"
+      />
+    </Box>
   );
 };
 
