d9cc741535bd70ee85baa4e4a7d04da69bcc5de0
Josh Holmes <josh@glowingmanagement.com>
2022-09-12 05:32:06 +0100

    upload field

--- src/components/DemoSongUploader/DemoSongUploader.js
+++ src/components/DemoSongUploader/DemoSongUploader.js
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import { Box, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 const DemoSongUploader = () => {
+  const [buttonPressed, setButtonPressed] = useState(false);
   const uploadedTracks = [];
+
+  const toggleButton = () => {
+    setButtonPressed(!buttonPressed);
+  };
+
   return (
     <Box
       sx={{
-        mt: "25%",
+        mt: "10%",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
       }}
     >
-      <Button variant="contained" startIcon={<AddIcon />}>
-        New Track
-      </Button>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <TextField
+          id="outlined-basic"
+          label="Spotify URI"
+          variant="outlined"
+          sx={{ mb: "15px", width: "400px" }}
+        />
+        <Button
+          variant="contained"
+          startIcon={<AddIcon />}
+          onClick={toggleButton}
+        >
+          Upload
+        </Button>
+      </Box>
+
       {uploadedTracks.length === 0 ? (
         <Typography sx={{ m: 2, textAlign: "center" }}>
           No Uploaded Tracks
