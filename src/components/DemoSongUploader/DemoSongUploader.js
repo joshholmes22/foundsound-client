c3d5b7e38b59a3013d4857d0cfd8dde487141198
Josh Holmes <josh@glowingmanagement.com>
2022-09-12 05:26:35 +0100

    add button and tracks array

--- src/components/DemoSongUploader/DemoSongUploader.js
+++ src/components/DemoSongUploader/DemoSongUploader.js
@@ -1,8 +1,29 @@
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import { Box, Typography } from "@mui/material";
+
 const DemoSongUploader = () => {
+  const uploadedTracks = [];
   return (
-    <div>
-      <h1>Demo Songs</h1>
-    </div>
+    <Box
+      sx={{
+        mt: "25%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Button variant="contained" startIcon={<AddIcon />}>
+        New Track
+      </Button>
+      {uploadedTracks.length === 0 ? (
+        <Typography sx={{ m: 2, textAlign: "center" }}>
+          No Uploaded Tracks
+        </Typography>
+      ) : (
+        <Typography sx={{ m: 2, textAlign: "center" }}>Tracks Here</Typography>
+      )}
+    </Box>
   );
 };
 
