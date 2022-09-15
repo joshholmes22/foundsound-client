600dfd03cf8a71d57e7571c58dd18648eea1e38a
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-09-15 17:06:25 +0100

    set up My Adverts page with h1

---
+++ src/containers/AllAds/AllAds.js
@@ -0,0 +1,5 @@
+const AllAds = () => {
+  return <h1>HELLO ALL ADS HERE</h1>;
+};
+
+export default AllAds;
--- src/pages/Ads/Ads.js
+++ src/pages/Ads/Ads.js
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material";
 import EventOrganiserNavBar from "../../components/EventOrganiserNavBar/EventOrganiserNavBar";
+import AllAds from "../../containers/AllAds";
 
 const Ads = () => {
   return (
     <>
       <EventOrganiserNavBar />
+      <AllAds />
     </>
   );
 };
