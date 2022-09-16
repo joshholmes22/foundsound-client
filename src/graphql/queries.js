01e83b1cefed1c85f30a9190225bf9cc71e1bbb8
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-16 21:09:19 +0100

    wip

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,3 +1,71 @@
+import { gql } from "@apollo/client";
+
+export const ADDRESS_LOOKUP = gql`
+  query Query($postcode: String!) {
+    addressLookup(postcode: $postcode) {
+      postcode
+      latitude
+      longitude
+      addresses {
+        formatted_address
+        thoroughfare
+        building_name
+        sub_building_name
+        sub_building_number
+        building_number
+        line_1
+        line_2
+        line_3
+        line_4
+        locality
+        town_or_city
+        county
+        district
+        country
+        _id
+        fullAddress
+      }
+    }
+  }
+`;
+
+export const GET_ALL_EVENTS = gql`
+  query Query {
+    getAllEvents {
+      id
+      name
+      description
+      address {
+        _id
+        fullAddress
+      }
+      postcode
+      startDate
+      endDate
+      startTime
+      endTime
+      imageUrl
+      tags {
+        name
+      }
+      adverts {
+        event
+        isPaid
+        expires
+      }
+      eventOwner {
+        id
+        firstName
+        lastName
+        email
+        imageUrl
+        socialMedia
+        userType
+      }
+    }
+  }
+`;
+
 export const GET_ARTIST_BY_ID = gql`
   query GetArtist($artistId: ID!) {
     getArtist(artistId: $artistId) {
