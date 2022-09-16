a8dc2fd937abecbd025d0e7924bed012611cdfc3
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-09-16 16:44:58 +0100

    home title

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,9 +1,11 @@
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import "./HeroBanner.css";
 
 const HeroBanner = () => {
+  const isMobile = useMediaQuery("(font-size:20px)");
   return (
     <>
       <Box
@@ -22,8 +24,9 @@ const HeroBanner = () => {
           FOUNDSOUND
         </Typography>
         <Typography
+          isMobile={isMobile}
           className="home-para"
-          sx={{ color: "#ffffff", fontSize: "2vw", fontWeight: "600" }}
+          sx={{ color: "#ffffff", fontSize: "3vw", fontWeight: "600" }}
         >
           Discover underground artists and upcoming events
         </Typography>
