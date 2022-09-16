256561acfb35e76d0abae759b47561099f943830
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-16 16:54:47 +0100

    wip

--- src/pages/ViewAnAd/ViewAnAd.js
+++ src/pages/ViewAnAd/ViewAnAd.js
@@ -1,6 +1,10 @@
+import { useQuery } from "@apollo/client";
+
 import { Typography } from "@mui/material";
 
 const ViewAnAd = () => {
+  const { data, loading, error } = useQuery(GET_ALL_ADS_FOR_OWNER);
+
   return <Typography> View an ad</Typography>;
 };
 
