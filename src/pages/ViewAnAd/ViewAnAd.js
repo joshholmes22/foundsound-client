2378ebea1c70d1b1782c0b72e9fcf1b4d7178909
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-09-16 16:56:29 +0100

    created view ads

--- src/pages/ViewAnAd/ViewAnAd.js
+++ src/pages/ViewAnAd/ViewAnAd.js
@@ -3,9 +3,12 @@ import { useQuery } from "@apollo/client";
 import { Typography } from "@mui/material";
 
 const ViewAnAd = () => {
-  const { data, loading, error } = useQuery(GET_ALL_ADS_FOR_OWNER);
+  //   const { data, loading, error } = useQuery(GET_ALL_ADS_FOR_OWNER);
+  //   check data
 
   return <Typography> View an ad</Typography>;
+  //   get a card and map through the artist details on
+  // if
 };
 
 export default ViewAnAd;
